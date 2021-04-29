1fee285d126eca651baf6be8e09549db28837f2f
Fernando Ferreira <fernando.ferreira.tbe@gmail.com>
2021-04-29 08:38:31 -0300

    Update service to return agendamento emails from db

--- src/main/java/io/github/fernandoferreira/servico/AgendamentoEmailServico.java
+++ src/main/java/io/github/fernandoferreira/servico/AgendamentoEmailServico.java
@@ -4,14 +4,19 @@ import java.util.List;
 
 import javax.ejb.Stateless;
 
+import io.github.fernandoferreira.dao.AgendamentoEmailDAO;
+import io.github.fernandoferreira.entidade.AgendamentoEmail;
+
 /* Stateless indica que não queremos guardar estado desta classe/objeto.
  * A cada requisição do usuário devera ser oferecida uma nova instância
  */
 @Stateless 
 public class AgendamentoEmailServico {
 	
-	public List<String> listar() {
-		return List.of("joao@me.com.br", "maria@me.com.br");
+	public List<AgendamentoEmail> listar() {
+		AgendamentoEmailDAO dao = new AgendamentoEmailDAO();
+		
+		return dao.listar();
 	}
 
 }
