eff9a7490d4482a2ded22853ce01c892f2fe5756
Fernando Ferreira <fernando.ferreira.tbe@gmail.com>
2021-05-01 19:15:22 -0300

    Inject dao bean

--- src/main/java/io/github/fernandoferreira/servico/AgendamentoEmailServico.java
+++ src/main/java/io/github/fernandoferreira/servico/AgendamentoEmailServico.java
@@ -3,20 +3,21 @@ package io.github.fernandoferreira.servico;
 import java.util.List;
 
 import javax.ejb.Stateless;
+import javax.inject.Inject;
 
 import io.github.fernandoferreira.dao.AgendamentoEmailDAO;
 import io.github.fernandoferreira.entidade.AgendamentoEmail;
 
-/* Stateless indica que não queremos guardar estado desta classe/objeto.
- * A cada requisição do usuário devera ser oferecida uma nova instância
+/* Stateless indica que nao queremos guardar estado desta classe/objeto.
+ * A cada requisicao do usuario devera ser oferecida uma nova instancia
  */
 @Stateless 
 public class AgendamentoEmailServico {
 	
+	@Inject
+	private AgendamentoEmailDAO dao;
+	
 	public List<AgendamentoEmail> listar() {
-		AgendamentoEmailDAO dao = new AgendamentoEmailDAO();
-		
 		return dao.listar();
 	}
-
 }
