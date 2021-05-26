4930c084480f5b55918a5d727dce5f216c799c14
Fernando Ferreira <fernando.ferreira.tbe@gmail.com>
2021-05-26 17:21:30 -0300

    Add support to lombok project

--- src/main/java/io/github/fernandoferreira/entidade/AgendamentoEmail.java
+++ src/main/java/io/github/fernandoferreira/entidade/AgendamentoEmail.java
@@ -7,6 +7,9 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import lombok.Data;
+
+@Data
 @Entity
 public class AgendamentoEmail implements Serializable {
 
@@ -23,63 +26,5 @@ public class AgendamentoEmail implements Serializable {
 	private String mensagem;
 	
 	private Boolean agendado;
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public String getAssunto() {
-		return assunto;
-	}
-
-	public void setAssunto(String assunto) {
-		this.assunto = assunto;
-	}
-
-	public String getMensagem() {
-		return mensagem;
-	}
-
-	public void setMensagem(String mensagem) {
-		this.mensagem = mensagem;
-	}
-
-	public Boolean getAgendado() {
-		return agendado;
-	}
-
-	public void setAgendado(Boolean agendado) {
-		this.agendado = agendado;
-	}
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		AgendamentoEmail other = (AgendamentoEmail) obj;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		return true;
-	}
-	
 	
 }
