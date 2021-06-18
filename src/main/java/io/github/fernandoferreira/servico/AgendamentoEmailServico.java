759ce1114b766111b2cb55a37fb7d5372b8ee3bb
Fernando Ferreira <fernando.ferreira.tbe@gmail.com>
2021-06-18 11:29:16 -0300

    Remove unused imports

--- src/main/java/io/github/fernandoferreira/servico/AgendamentoEmailServico.java
+++ src/main/java/io/github/fernandoferreira/servico/AgendamentoEmailServico.java
@@ -4,8 +4,6 @@ import java.util.List;
 import java.util.logging.Logger;
 
 import javax.ejb.Stateless;
-import javax.ejb.TransactionAttribute;
-import javax.ejb.TransactionAttributeType;
 import javax.inject.Inject;
 
 import io.github.fernandoferreira.dao.AgendamentoEmailDAO;
